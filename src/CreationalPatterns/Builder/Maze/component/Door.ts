ce351e754eeb48166a324b20aafd769aa1c1371e
kaddo <kofi.addo@facile.it>
2020-06-05 20:23:02 +0200

    feat: door component output

--- src/CreationalPatterns/Builder/Maze/component/Door.ts
+++ src/CreationalPatterns/Builder/Maze/component/Door.ts
@@ -1,10 +1,13 @@
 export class Door {
     private static doorCount = 1;
     #doorNumber: number;
+
     constructor(private roomFrom: number, private roomTo: number) {
         this.#doorNumber = Door.doorCount++;
         console.log(
-            `Creating a Door# ${this.#doorNumber} between ${this.roomFrom.toString()} and ${this.roomTo.toString()}`,
+            `Creating a Door# ${
+                this.#doorNumber
+            } between room# ${this.roomFrom.toString()} and room# ${this.roomTo.toString()}`,
         );
     }
 
